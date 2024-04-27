c9c7a74b0215f19118b0d3e3f4f9b9a872454521
robmars123 <robmars2008@gmail.com>
2024-04-26 22:54:22 -0500

    Moved Views to Shared Project

---
+++ Calculator.Shared/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator.Shared.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+
+}
